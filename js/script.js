62ed5b40d1bd155f3dd4ffed44a3d28604efae7e
Sergey <xnesart@mail.ru>
2022-03-28 00:06:47 +0300

    perviy commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr--);
+console.log(decr++);
+
+const isCkecked = true,
+        isCLose = true;
+
+        console.log(isCkecked && isClose);
