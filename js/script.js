9719365e5d54e47bc3ca57d45902d5de22a0ae26
Sergey <xnesart@mail.ru>
2022-03-28 12:09:18 +0300

    выполнен первый урок

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 "use strict";
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "")
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const looked1 = prompt("Один из последних просмотренных фильмов?", "");
+const value1 = prompt("На сколько оцените фильм?", "");
+const looked2 = prompt("Один из последних просмотренных фильмов?", "");
+const value2 = prompt("На сколько оцените фильм?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+personalMovieDB.movies[looked1] = value1;
+personalMovieDB.movies[looked2] = value2;
+console.log(personalMovieDB);
