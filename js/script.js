c948c9c5db950c2bb2d4b3a9d0816e7e9edf3135
Sergey <xnesart@mail.ru>
2022-04-04 17:00:01 +0300

    урок 16, функции

--- js/script.js
+++ js/script.js
@@ -68,11 +68,42 @@
 //     }
 //     console.log(i);
 // }
+//========================================================================================================================================================
+
+// for (let i = 0; i < 2; i++) {
+//   let numberOfFilms = prompt("Сколько фильмов вы посмотрели?", "");
+//   const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+//   };
+//   let lookedFilm = prompt("Один из последних просмотренных фильмов?", "");
+//   let filmRate = prompt("Ваша оценка?", "");
+//   if (lookedFilm != null && filmRate != null && lookedFilm.length < 50 && lookedFilm != "" && filmRate != "") {
+//     personalMovieDB.movies[lookedFilm] = filmRate;
+//   } else {
+//     console.log("повторно введите ваш ответ");
+//     i--;
+//   }
+
+//   if (personalMovieDB.count < 10) {
+//     console.log("Просмотрено немного фильмов");
+//   } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+//     console.log("Вы классический зритель");
+//   } else if (personalMovieDB.count >= 30) {
+//     console.log("вы заядлый киноман мазафака");
+//   } else {
+//     console.log("Произошла ошибка");
+//   }
+//   console.log(personalMovieDB);
+// }
+//========================================================================================================================================================
 
 let numberOfFilms = prompt("Сколько фильмов вы посмотрели?", "");
-let lookedFilm;
-let filmRate;
-
+let lookedFilm = prompt("Один из последних просмотренных фильмов?", "");
+let filmRate = prompt("Ваша оценка?", "");
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -81,25 +112,61 @@ const personalMovieDB = {
   privat: false,
 };
 
-for (let i = 1; i <= 2; i++) {
-  lookedFilm = prompt("Один из последних просмотренных фильмов?", "");
-  if (lookedFilm === 0 || lookedFilm > 50) {
-    console.log("неподходящая длина ответа");
-    break;
-  } else if (lookedFilm === null) {
-    console.log("отмена");
-    break;
-  }
-  filmRate = prompt("На сколько оцените фильм?", "");
+do {
   personalMovieDB.movies[lookedFilm] = filmRate;
-  if (personalMovieDB.count < 10) {
-    console.log("Просмотрено немного фильмов");
-  } else if (personalMovieDB.count > 10 < 30) {
-    console.log("Вы классический зритель");
-  } else {
-    console.log("вы заядлый киноман мазафака");
-  }
-}
+  console.log(personalMovieDB);
+} while (lookedFilm != null && filmRate != null && lookedFilm.length < 50 && lookedFilm != "" && filmRate != "");
+
+//========================================================================================================================================================
+
+// let numberOfFilms = prompt("Сколько фильмов вы посмотрели?", "");
+
+// let personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
+// let lastFilm = prompt("Последний фильм?", ""),
+//   filmRate = prompt("Ваша оценка?", "");
+// lastFilm;
+// filmRate;
+// console.log(personalMovieDB);
 // personalMovieDB.movies[lookedFilm] = filmRate;
 
-console.log(personalMovieDB);
+let num = 20;
+
+function showFirstMessage(text) {
+  console.log(text);
+  num = 10;
+}
+showFirstMessage("Hello World!");
+console.log(num);
+
+function calc(a, b) {
+  return a + b;
+}
+
+console.log(calc(4, 3));
+console.log(calc(7, 5));
+console.log(calc(3, 11));
+
+function ret() {
+  let num = 50;
+  //
+  return num;
+}
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function () {
+  console.log("Hello");
+};
+logger();
+
+const calc = (a, b) => a + b;
+const calc = (a, b) => {
+  console.log("1");
+  return a + b;
+};
