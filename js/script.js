273894ae40632b32d8414ac206249e70d05231a5
xnesart <60525875+xnesart@users.noreply.github.com>
2022-04-15 17:17:13 +0300

    29 действия с элементами на странице

--- js/script.js
+++ js/script.js
@@ -1,28 +1,67 @@
 "use strict";
 
-// Получение элемента по id
-const box = document.getElementById('box');
-console.log(box);
-// Получение элемента по названию тегов
-const btns = document.getElementsByTagName('button'); // Тут мы получили не один элемент, а псевдомассив
-console.log(btns);
-// Получаем конкретную кнопку
-const btns = document.getElementsByTagName('button')[1];
-console.log(btns);
-// Меняем стиль конкретной кнопки
-console.log(btns[0]);//Обратились к конкретной кнопке
+const box = document.getElementById("box"),
+  btns = document.getElementsByTagName("button"),
+  circles = document.getElementsByClassName("circle"),
+  hearts = document.querySelectorAll(".heart"),
+  oneHeart = document.querySelector(".heart");
 
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
+console.dir(box);
+box.style.backgroundColor = "blue";
+box.style.width = "500px";
+btns[1].style.borderRadius = "100%";
+circles[0].style.backgroundColor = "red";
 
-// Современные методы
-// В скобки помещаем css селектор
-const hearts = document.querySelectorAll('.heart'); // Он поддерживает все методы, которые были выше. У него есть метод forEach, хотя он является псевдомассивом
-console.log(hearts);
-hearts.forEach(item => {
-  console.log(item);
+// Назначение нескольких inline стилей
+box.style.cssText = "background-color: aqua; width: 500px";
+
+// Над несколькими элементами
+// 1
+for (let i = 0; i < hearts.length; i++) {
+  hearts[i].style.backgroundColor = "blue";
+}
+// 2
+hearts.forEach((item) => {
+  item.style.backgroundColor = "blue";
 });
+// Создаем элемент, он существует только внутри js, он не появится на странице
+const div = document.createElement("div");
+//Используется редко
+const text = document.createTextNode("Тут был я");
 
-// Получаем первый попавшийся элемент
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+//Создаем элемент и добавляем класс
+div.classList.add("black");
+//Добавляем наш div в конец body. Тупо выбираем родителя и ему добавляем элемент
+document.body.append(div);
+// 2
+document.querySelector(".wrapper").append(div);
+const box = document.getElementById("box"),
+  btns = document.getElementsByTagName("button"),
+  circles = document.getElementsByClassName("circle"),
+  hearts = document.querySelectorAll(".heart"),
+  oneHeart = document.querySelector(".heart");
+  wrapper = querySelector(".wrapper");
+
+  wrapper.append(div);
+wrapper.prepend(div);//помещает в начало родителя
+wrapper.appendChild(div); //То же самое
+
+wrapper.insertBefore(div, hearts[0]);// Это старое добавление
+
+// Помещаем div перед элементом
+hearts[0].before(div);
+// Помещаем div после элемента
+hearts[0].after(div);
+
+// Удаляем элемент
+circles[0].remove();
+wrapper.removeChild(hearts[1]);//Раньше удаляли через родителя
+
+
+// замена элемента
+hearts[0].replaceWith(circles[0]);
+wrapper.replaceChild(circles[0], hearts[0]);//Первый элемент меняется на второй. Устаревшее
+
+div.innerHTML = '<h1>hello world</h1>';// Тут добавится html структура - теги h1
+div.textContent = 'Hello'; // Тут просто текст добавится
+div.insertAdjacentHTML("beforebegin", '<h2>Hello</h2>')// beforebegin afterbegin beforeend
