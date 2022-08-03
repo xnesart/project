8c8905e3dae38f6b610cb4108b0a2700fe30d1b8
xnesart <60525875+xnesart@users.noreply.github.com>
2022-08-03 13:48:11 +0300

    урок 29

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 "use strict";
 
 const box = document.getElementById("box");
-console.log(box);
-const btns = document.getElementsByTagName("button")[1];
-console.log(btns);
+const btns = document.getElementsByTagName("button");
 const circles = document.getElementsByClassName("circle");
-console.log(circles);
 const hearts = document.querySelectorAll(".heart");
-// console.log(hearts);
+const oneHeart = document.querySelector(".heart");
+const wrapper = document.querySelector(".wrapper");
+const div = document.createElement("div");
+
+div.classList.add("black");
+wrapper.insertBefore(div, hearts[1]);
+
+// box.style.backgroundColor = 'blue';
+box.style.cssText = "background-color: brown; border-radius: 300px; height: 100px; display: none";
+box.style.width = "500px";
+
+btns[1].style.borderRadius = "100%";
+// красим элементы с помощью цикла
+for (let i = 0; i < hearts.length; i++) {
+    hearts[i].style.backgroundColor = "pink";
+}
+
 hearts.forEach((item) => {
-    console.log(item);
+    item.style.backgroundColor = "red";
 });
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart)
+circles[0].remove();
