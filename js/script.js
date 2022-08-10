38ec4e98934acd582b7ee7fc6fb4ec87f925153c
xnesart <60525875+xnesart@users.noreply.github.com>
2022-08-10 12:58:48 +0300

    урок 38 создаем табы v2

--- js/script.js
+++ js/script.js
@@ -1,38 +1,42 @@
 window.addEventListener("DOMContentLoaded", (e) => {
     const tabsParent = document.querySelector(".tabheader__items"),
-        tabsContent = document.querySelectorAll(".tabcontent"),
         tabs = document.querySelectorAll(".tabheader__item"),
-        tabActive = document.querySelector(".tabheader__item_active");
+        tabActive = document.querySelector(".tabheader__item_active"),
+        tabContent = document.querySelectorAll(".tabcontent");
 
-    function hideTabContent() {
-        tabsContent.forEach((content) => {
-            content.classList.add("hide");
-            content.classList.remove("show", "fade");
-        });
+    function hideTabs() {
         tabs.forEach((tab) => {
             tab.classList.remove("tabheader__item_active");
         });
+        tabContent.forEach((item) => {
+            item.classList.add("hide");
+            item.classList.remove("show", "fade");
+        });
+        tabActive.classList.remove("tabheader__item_active");
     }
-    function showTabContent(i = 0) {
-        tabsContent[i].classList.add("show", "fade");
-
-        tabsContent[i].classList.remove("hide");
+    function showTabs(i = 0) {
         tabs[i].classList.add("tabheader__item_active");
+        tabContent[i].classList.add("show", "fade");
+        tabContent[i].classList.remove("hide");
     }
+
     tabsParent.addEventListener("click", (event) => {
         const target = event.target;
-
         if (target && target.classList.contains("tabheader__item")) {
             tabs.forEach((item, i) => {
                 if (item == target) {
-                    hideTabContent();
-                    showTabContent(i);
+                    hideTabs();
+                    showTabs(i);
+                    console.log('click')
                 }
             });
         }
     });
-
-    hideTabContent();
-
-    showTabContent();
+    hideTabs();
+    showTabs();
 });
+
+//         tabsParent.addEventListener('click', (event) =>{
+//             const target = event.target;
+
+// });
