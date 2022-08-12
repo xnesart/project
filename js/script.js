a1d818a72b8ffef77c509b11322e04d0d3661e3b
xnesart <60525875+xnesart@users.noreply.github.com>
2022-08-12 13:41:54 +0300

    урок 40 работа с датами

--- js/script.js
+++ js/script.js
@@ -1,42 +1,14 @@
-const btn = document.querySelector(".btn");
-let timerId, // Объявляем переменную таймера, чтобы её можно было использовать везде
-    i = 0; // объявляем переменную счетчика
-function myAnimation() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
+// const now = new Date('2020-05-01');
+//     new Date.parse('2020-0501'); // будут идентичны
+// console.log(now.setHours(18));
+// console.log(now)
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
 
-const id = setInterval(frame, 10);
-
-    function frame(){
-        if (pos === 300){
-            clearInterval(id)
-        } else{
-            pos++;
-            elem.style.top = pos + "px";
-            elem.style.left = pos + "px";
-        }
-    }
+let start = new Date();
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3; //две зведочки это возведение в степень, появился в es6
 }
-
-btn.addEventListener('click', myAnimation);
-
-
-// btn.addEventListener("click", () => {
-//     // const timerId = setTimeout(logger, 2000);  Устанавливает таймер, после которого запустится событие
-//     timerId = setInterval(logger, 2000); // Устанавливает интервал. Событие будет происходить постоянно каждые 2 сек
-// });
-
-// Еще один вариант таймера, внутрь передается функция
-
-// function logger() {
-//     if (i === 3) {
-//         clearInterval(timerId); // Останавливаем таймер
-//     }
-//     console.log("text");
-//     i++;
-// }
-
-// let id = setTimeout(function log() {
-//     console.log("hello");
-//     id = setTimeout(log, 500);// потом выполняет эту функцию
-// }, 500); // сначала ждет тут // потом опять ждет тут и заново выполняет верхнюю
+let end = new Date();
+alert(`цикл отработал за ${end - start} милисекунд`);
